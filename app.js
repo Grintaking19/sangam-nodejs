806bb06e424722e9bf2cf2fbe3c38ca4782b0ade
Grintaking19 <omarbadrh.ob@gmail.com>
2026-03-30 14:13:05 +0200

    Finished Basic API, With NO MVC

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 // Mock Data
-const books = [
+let books = [
   { id: 1, title: "The Great Gatsby", author: "F. Scott Fitzgerald" },
   { id: 2, title: "To Kill a Mockingbird", author: "Harper Lee" },
   { id: 3, title: "1984", author: "George Orwell" },
@@ -24,6 +24,69 @@ app.get("/", (req, res) => {
 
 app.get("/books", (req, res) => {
   res.json(books);
+});
+
+app.get("/books/:id", (req, res) => {
+  const getSingleBookId = books.find(
+    (book) => book.id === parseInt(req.params.id),
+  );
+  if (getSingleBookId) {
+    res.json(getSingleBookId);
+  } else {
+    res.status(404).json({
+      message: "Book Not Found",
+    });
+  }
+});
+
+// Create a new Book
+app.post("/books", (req, res) => {
+  const newBook = req.body;
+  newBook.id = crypto.randomUUID();
+  books.push(newBook);
+  res.status(200).json({
+    status: "success",
+    data: newBook,
+  });
+});
+
+// Update a book
+app.put("/books/:id", (req, res) => {
+  const getSingleBook = books.find(
+    (item) => item.id === parseInt(req.params.id),
+  );
+  if (getSingleBook) {
+    const title = req.body?.title || getSingleBook.title;
+    const author = req.body?.author || getSingleBook.author;
+    res.status(200).json({
+      status: "success",
+      data: {
+        id: getSingleBook.id,
+        title: title,
+        author: author,
+      },
+    });
+  } else {
+    res.status(404).json({
+      message: "Book Not Found, Check the ID",
+    });
+  }
+});
+
+// Delete a book
+app.delete("/books/:id", (req, res) => {
+  const getSingleBook = books.find(
+    (item) => item.id === parseInt(req.params.id),
+  );
+  if (getSingleBook) {
+    const newBooks = books.filter((book) => book.id !== getSingleBook.id);
+    res.status(200).json({
+      status: "success",
+      data: newBooks,
+    });
+  } else {
+    res.status(404).json({ message: "Book you Want to delete is Not Found" });
+  }
 });
 
 app.listen(PORT, () => {
