1453316bb50bc91186f025fc1abba4aa446a0357
Grintaking19 <omarbadrh.ob@gmail.com>
2026-04-07 04:51:07 +0200

    Added the ability to store images locally using multer

---
+++ src/middleware/uploadMiddleware.js
@@ -0,0 +1,37 @@
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname),
+    );
+  },
+});
+
+// File filter to allow only images and video files
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = /jpeg|jpg|png|gif|mp4|avi|mkv/;
+  const extname = allowedTypes.test(
+    path.extname(file.originalname).toLowerCase(),
+  );
+  const mimetype = allowedTypes.test(file.mimetype);
+  if (extname && mimetype) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only image and video files are allowed!"), false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+  limits: { fileSize: 5 * 1024 * 1024 }, // Limit file size to 5MB
+});
+
+export default upload;
--- src/routes/image-routes.js
+++ src/routes/image-routes.js
@@ -2,11 +2,11 @@ import express from "express";
 import { uploadImage, getAllImages } from "../controllers/image-controllers.js";
 import authMiddleware from "../middleware/authMiddleware.js";
 import adminMiddleware from "../middleware/adminMiddleware.js";
-
+import uploadMiddleware from "../middleware/uploadMiddleware.js";
 const router = express.Router();
 
 // Only admin users can upload images
-router.post("/upload", authMiddleware, adminMiddleware, uploadImage);
+router.post("/upload", authMiddleware, adminMiddleware, uploadMiddleware.single("image"), uploadImage);
 
 // Both admin and regular users can view all images
 router.get("/", authMiddleware, getAllImages);
